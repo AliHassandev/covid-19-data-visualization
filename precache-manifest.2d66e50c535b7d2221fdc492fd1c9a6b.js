15b9afc0c6003f6f4e0248b41f6ef06d15626820
dev.alihassan@gmail.com <dev.alihassan@gmail.com>
2020-04-19 21:45:02 +0300

    Updates

---
+++ precache-manifest.2d66e50c535b7d2221fdc492fd1c9a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ced0fe3a102bc76a77bd86ac5f91882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2064f5c447b32bf2d66",
+    "url": "/static/css/main.56fffce0.chunk.css"
+  },
+  {
+    "revision": "63b42d07305c9049f6d6",
+    "url": "/static/js/2.34ab1bc4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.34ab1bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2064f5c447b32bf2d66",
+    "url": "/static/js/main.5bb69fe3.chunk.js"
+  },
+  {
+    "revision": "26c344042a6c81fbc474",
+    "url": "/static/js/runtime-main.cf736b67.js"
+  }
+]);
